97a88421b6932fe40647c8b101054b9508fe6c15
carlosjeferson <Carlosjjs500214@gmail.com>
2025-06-25 19:55:28 -0300

    atv

---
+++ atv-06/models/aluno.js
@@ -0,0 +1,10 @@
+class Aluno {
+    constructor(id, nome, curso, IRA) {
+        this.id = id;
+        this.nome = nome;
+        this.curso = curso;
+        this.IRA = IRA;
+    }
+};
+
+module.exports = Aluno;
